134c6ff391f082ea2deba882d39125e3ee22cac3
MMcClure11 <mmcclure0100@gmail.com>
2025-04-03 10:19:24 -0500

    feat: get product by code

--- test/elixir_market/products_test.exs
+++ test/elixir_market/products_test.exs
@@ -14,4 +14,30 @@ defmodule ElixirMarket.ProductsTest do
       assert Products.list_products() == expected_products
     end
   end
+
+  describe "get_product_by_code/1" do
+    test "returns the product for a valid code when given string" do
+      assert Products.get_product_by_code("CF1") == %Product{
+               code: :CF1,
+               name: "Coffee",
+               price: 11.23
+             }
+    end
+
+    test "returns the product for a valid code when given atom" do
+      assert Products.get_product_by_code(:CF1) == %Product{
+               code: :CF1,
+               name: "Coffee",
+               price: 11.23
+             }
+    end
+
+    test "returns nil for an invalid atom code" do
+      assert Products.get_product_by_code(:INVALID_CODE) == nil
+    end
+
+    test "returns nil for an invalid string code" do
+      assert Products.get_product_by_code("K9U") == nil
+    end
+  end
 end
